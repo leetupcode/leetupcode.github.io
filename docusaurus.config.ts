ef91b2de75f9e14c02e15fb298bc58619c0edfad
salilvnair <salilvnairgithub@gmail.com>
2024-06-15 14:22:44 -0500

    added website deploy config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,22 +3,26 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Leetupcode',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://leetupcode.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'leetupcode', // Usually your GitHub org/user name.
+  projectName: 'leetupcode.github.io', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+
+  deploymentBranch: 'deployment',
+  trailingSlash: false,
+
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
@@ -58,7 +62,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Leetupcode',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
